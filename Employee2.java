045c3b55e0f1d822de0b7dd36fd2d768b744bd7d
8088349357 <santu78634@gmail.com>
2025-04-29 09:45:48 +0530

    created

--- Employee2.java
+++ Employee2.java
@@ -4,19 +4,22 @@ public class Employee2 {
     String name;
     int id;
     double salary;
-        public Employee2(String name, int id, double salary){
+    String address;
+        public Employee2(String name, int id, double salary,String address){
             this.name = name;
             this.id = id;
             this.salary = salary;
+            this.address = address;
         }
         public void displayInfo(){
 
             System.out.println("Employee ID :" +id);
             System.out.println("Employee Name :" +name);
             System.out.println("Salary: $" + salary);
+            System.out.println("Address :" +address);
         }
         public static void main(String[] args){
-            Employee2 emp1 = new Employee2("john",1234,2000);
+            Employee2 emp1 = new Employee2("john",1234,2000,"Hubbali");
             emp1.displayInfo();
         }
     }
